7776c118d661c32aa296916665fbcaf16dec7495
nelson <baruica+dev@gmail.com>
2013-08-08 12:14:54 +0200

    removed use

--- src/Srm/WebsiteBundle/Form/Type/Address/ZipType.php
+++ src/Srm/WebsiteBundle/Form/Type/Address/ZipType.php
@@ -2,7 +2,6 @@
 
 namespace Srm\WebsiteBundle\Form\Type\Address;
 
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
