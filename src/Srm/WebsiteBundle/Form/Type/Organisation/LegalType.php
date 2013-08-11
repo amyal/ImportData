b17c61efadadaa010b5f7a1f00b7d003e290a1f6
nelson <baruica+dev@gmail.com>
2013-08-12 00:17:17 +0200

    partial work on organisation legal

---
+++ src/Srm/WebsiteBundle/Form/Type/Organisation/LegalType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Srm\WebsiteBundle\Form\Type\Organisation;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LegalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'label', 'text', array(
+                    'label' => 'form.organisation.basic.label',
+                    'attr'  => array('autofocus' => 'autofocus')
+                )
+            )
+            ->add('address', 'srm_address')
+            ->add(
+                'identificationCode', 'text', array(
+                    'label'     => 'form.organisation.basic.identification_code',
+                    'read_only' => true
+                )
+            )
+            ->add('slogan1', 'text', array('label' => 'form.organisation.basic.slogan1'))
+            ->add('slogan2', 'text', array('label' => 'form.organisation.basic.slogan2'))
+            ->add('slogan3', 'text', array('label' => 'form.organisation.basic.slogan3'))
+
+            ->add(
+                'logo', 'file', array(
+                    'label'    => 'form.organisation.basic.logo',
+                    'required' => false
+                )
+            )
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound'   => true,
+            'data_class' => 'Srm\CoreBundle\Entity\Organisation',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'srm_organisation_legal';
+    }
+}
