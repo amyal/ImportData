99359e7b327d1f0f8df2aa8af51b4d2b0baf5b3c
nelson <baruica+dev@gmail.com>
2013-08-26 17:58:16 +0200

    first commit for Departments

--- src/Srm/WebsiteBundle/Controller/DepartmentsController.php
+++ src/Srm/WebsiteBundle/Controller/DepartmentsController.php
@@ -4,6 +4,7 @@ namespace Srm\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Srm\CoreBundle\Entity\Department;
 use Srm\CoreBundle\Entity\Organisation;
 use Srm\CoreBundle\Entity\Site;
 
@@ -13,24 +14,29 @@ class DepartmentsController extends Controller
     {
         return $this->render('SrmWebsiteBundle:Department:list.html.twig', array(
             'organisation' => $organisation,
-            'departments'  => $this->getDoctrine()->getRepository('Srm\CoreBundle\Entity\Department')->findByOrganisation($organisation)
+            'departments'  => $this->getDoctrine()->getRepository('Srm\CoreBundle\Entity\Department')->findBySites(array($site))
         ));
     }
 
-    public function formAction(Organisation $organisation, Site $site, Department $department)
+    public function showAction(Organisation $organisation, Department $department)
+    {
+        return $this->render('SrmWebsiteBundle:Department:show.html.twig', array(
+            'organisationId' => $organisation->getOrganisationId(),
+            'department'     => $department,
+        ));
+    }
+
+    public function formAction(Organisation $organisation, Department $department)
     {
         $formActionRoute = 'srm_website_departments_add';
-        $formActionRouteParams = array(
-            'organisationId' => $organisation->getOrganisationId(),
-            'siteId'         => $site->getSiteId(),
-        );
+        $formActionRouteParams = array('organisationId' => $organisation->getOrganisationId());
 
         if (null !== $departmentId = $department->getDepartmentId()) {
-            $formActionRoute = 'srm_website_departments_edit';
+            $formActionRoute = 'srm_website_department_edit';
             $formActionRouteParams['departmentId'] = $departmentId;
         }
 
-        $form = $this->createForm('srm_site', $site, array(
+        $form = $this->createForm('srm_department', $department, array(
             'action' => $this->generateUrl($formActionRoute, $formActionRouteParams),
             'method' => 'POST',
             'attr'   => array('class' => 'form-horizontal', 'novalidate' => 'novalidate'),
@@ -39,24 +45,24 @@ class DepartmentsController extends Controller
         $request = $this->getRequest();
 
         if ('GET' === $request->getMethod()) {
-            return $this->render('SrmWebsiteBundle:Site:form.html.twig', array(
-                'form'           => $form->createView(),
+            return $this->render('SrmWebsiteBundle:Department:form.html.twig', array(
                 'organisationId' => $organisation->getOrganisationId(),
+                'form'           => $form->createView(),
             ));
         }
 
         if (false === $form->handleRequest($request)->isValid()) {
-            return $this->render('SrmWebsiteBundle:Site:form.html.twig', array(
-                'form'           => $form->createView(),
+            return $this->render('SrmWebsiteBundle:Department:form.html.twig', array(
                 'organisationId' => $organisation->getOrganisationId(),
+                'form'           => $form->createView(),
             ));
         }
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($site);
+        $em->persist($department);
         $em->flush();
 
-        return $this->redirect($this->generateUrl('srm_website_sites_list', array(
+        return $this->redirect($this->generateUrl('srm_website_departments_list', array(
             'organisationId' => $organisation->getOrganisationId(),
         )));
     }
---
+++ src/Srm/WebsiteBundle/Form/Type/Department/DepartmentType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Srm\WebsiteBundle\Form\Type\Department;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DepartmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'label', 'text', array(
+                    'label'    => 'department.label',
+                    'required' => true,
+                    //'attr'     => array('autofocus' => 'autofocus'),
+                )
+            )
+
+            ->add('subDepartments', 'srm_sub_department')
+
+            //->add('sites', 'srm_department_site')
+
+            ->add('contacts', 'srm_contact')
+
+            ->add('enabled', 'checkbox', array('label' => 'enabled'))
+
+            ->add('save', 'submit', array('label' => 'button.save'))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound'   => true,
+            'data_class' => 'Srm\CoreBundle\Entity\Department',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'srm_department';
+    }
+}
