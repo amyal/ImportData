7fbb9dfe47be4b0d61d92b5f53e116d36af53895
nelson <baruica+dev@gmail.com>
2013-09-02 00:16:10 +0200

    added a repository to Contact

---
+++ src/Srm/CoreBundle/Entity/Repository/ContactRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Srm\CoreBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+use Srm\CoreBundle\Entity\Organisation;
+
+class ContactRepository extends EntityRepository
+{
+    public function findNonDeletedByOrganisation(Organisation $organisation)
+    {
+        return $this->createQueryBuilder('c')
+            ->where('c.deleted != :deleted')->setParameter('deleted', true)
+            ->andWhere('c.organisation = :organisation')->setParameter('organisation', $organisation)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
