b6cc0d655e2c94db44cf0c9c894dd8c06f43dd11
nelson <baruica+dev@gmail.com>
2013-09-15 22:10:02 +0200

    added function to retrieve all Users

--- src/Srm/CoreBundle/Repository/ContactRepository.php
+++ src/Srm/CoreBundle/Repository/ContactRepository.php
@@ -18,4 +18,16 @@ class ContactRepository extends EntityRepository
             ->getResult()
         ;
     }
+
+    public function findNonDeletedUsersByOrganisation(Organisation $organisation)
+    {
+        return $this->createQueryBuilder('c')
+            ->where('c.deleted = :deleted')->setParameter('deleted', false)
+            ->andWhere('c.organisation = :organisation')->setParameter('organisation', $organisation)
+            ->andWhere('c.shareholder = :shareholder')->setParameter('shareholder', false)
+            ->andWhere('c.isUser = :isUser')->setParameter('isUser', true)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
