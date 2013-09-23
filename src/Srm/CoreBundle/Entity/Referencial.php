ea82256db5d042007420cefd6f538a71112c0284
Verseo <hichamelfassi@gmail.com>
2013-09-24 01:17:53 +0200

    rename organisationRepository by repository and rename vr_repository by vr_referencial and list form show indicators

---
+++ src/Srm/CoreBundle/Entity/Referencial.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Srm\CoreBundle\Entity;
+
+/**
+ * Referencial
+ */
+class Referencial
+{
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var boolean
+     */
+    private $enabled;
+
+    /**
+     * @var boolean
+     */
+    private $deleted;
+
+    /**
+     * @var integer
+     */
+    private $referencialId;
+
+
+    public function __construct()
+    {
+        $this->deleted = false;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return Referencial
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Referencial
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Referencial
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set deleted
+     *
+     * @param boolean $deleted
+     * @return Referencial
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted
+     *
+     * @return boolean
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * Get referencialId
+     *
+     * @return integer
+     */
+    public function getReferencialId()
+    {
+        return $this->referencialId;
+    }
+}
