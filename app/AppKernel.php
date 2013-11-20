304afdf5f7fed836d7bcb731b3d1991564a30d79
verseo <hichamelfassi+verseo@gmail.com>
2013-11-20 17:12:01 +0100

    Rachid : j'ai déclaré le bundle  Swiftmailer pour faire pour envoyer des emails (enregistrement contact.. )

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,7 +13,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
