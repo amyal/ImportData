7ec706dee676059a06eb4a8f34c2d225ecff3391
verseo <hichamelfassi+verseo@gmail.com>
2013-11-21 09:39:03 +0100

    Rachid : rendre l'affichage du code normale, sans  liaison de la table Zip

--- src/Srm/WebsiteBundle/Form/Type/ZipType.php
+++ src/Srm/WebsiteBundle/Form/Type/ZipType.php
@@ -6,32 +6,22 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-use Srm\WebsiteBundle\Form\EventListener\ZipListener;
 
 class ZipType extends AbstractType
 {
-    private $listener;
-
-    public function __construct(ZipListener $listener)
-    {
-        $this->listener = $listener;
-    }
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
+     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code',    'text', array('label' => 'address.zip'))
+            ->add('code',    'text')
             ->add('city',    'srm_cities')
             ->add('country', 'srm_countries')
         ;
-
-        $builder->addEventSubscriber($this->listener);
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'coumpound'  => true,
+            'compound'  => true,
             'data_class' => 'Srm\CoreBundle\Entity\Zip',
         ));
     }
