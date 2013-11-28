9dbd7214700e2d693f6a4b56b33a0c10db0dea72
verseo <hichamelfassi+verseo@gmail.com>
2013-11-28 10:09:14 +0100

    FosUserBundle c'est pour la configuration de ce bundle

--- src/Srm/UserBundle/SrmUserBundle.php
+++ src/Srm/UserBundle/SrmUserBundle.php
@@ -5,4 +5,6 @@ namespace Srm\UserBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SrmUserBundle extends Bundle
-{}
+{public function getParent() 
+        {  
+    return 'FOSUserBundle';  }}
