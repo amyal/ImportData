f5bdd3e7dda526285e92dcecafe1467bdd7709d0
verseo <hichamelfassi+verseo@gmail.com>
2013-12-18 17:47:15 +0100

    Finaliser le formulaire des parties prenantes l'ajout du contact interne et externe

--- src/Srm/WebsiteBundle/Controller/ContactsController.php
+++ src/Srm/WebsiteBundle/Controller/ContactsController.php
@@ -3,9 +3,10 @@
 namespace Srm\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use Srm\CoreBundle\Entity\Contact;
 use Srm\CoreBundle\Entity\Organisation;
+use Srm\CoreBundle\Entity\Stakeholder;
 use Srm\UserBundle\Entity\User;
 
 class ContactsController extends Controller
@@ -38,29 +39,32 @@ class ContactsController extends Controller
         )));
     }
 
-    public function formAction(Organisation $organisation, Contact $contact)
+    public function formAction(Organisation $organisation,$type, Contact $contact)
     {   
+        $request = $this->getRequest();//récupérer les ids de partie prenante et la nouvelle organisation 
+        $stakeholderid = $request->query->get('stakeholderid'); 
+        $org_clt = $request->query->get('org_clt');
+        
         $formActionRoute = 'srm_website_contacts_add';
-        $formActionRouteParams = array('organisationId' => $organisation->getOrganisationId());
+        $formActionRouteParams = array('organisationId' => $organisation->getOrganisationId(),'type'=>$type,'stakeholderid'=>$stakeholderid, 'org_clt'=>$org_clt);
         $FormEdit=$contact->getContactId() ; //  if the variable equal Null -> add else edition 
         
         if (null !== $contactId = $contact->getContactId()) {
             $formActionRoute = 'srm_website_contact_edit';
             $formActionRouteParams['contactId'] = $contactId;
                  }
-
+                 
         $form = $this->createForm('srm_contact', $contact, array(
             'action' => $this->generateUrl($formActionRoute, $formActionRouteParams),
             'method' => 'POST',
             'attr'   => array('class' => 'form-horizontal', 'novalidate' => 'novalidate'),
         ));
 
-        $request = $this->getRequest();
 
         if ('GET' === $request->getMethod()) {
             return $this->render('SrmWebsiteBundle:Contact:form.html.twig', array(
                 'organisationId' => $organisation->getOrganisationId(),
-                'form'           => $form->createView(),
+                'form'           => $form->createView(),  
             ));
         }
 
@@ -70,12 +74,16 @@ class ContactsController extends Controller
                 'form'           => $form->createView(),
             ));
         }
-
+                    
+   if ($type=="externe"){ // liéer le contact à la nouvelle organisation et la partie prenante
+       $contact->addStakeholder($this->getDoctrine()->getRepository('Srm\CoreBundle\Entity\Stakeholder')->find($stakeholderid)); //mapping with stakeholder table
+       $contact->setOrganisation($this->getDoctrine()->getRepository('Srm\CoreBundle\Entity\Organisation')->find($org_clt));
+   }   
         $em = $this->getDoctrine()->getManager();
         $em->persist($contact);
         $em->flush();
-
-        if (true === $contact->getIsUser()) {
+                
+    if (true === $contact->getIsUser()) {
             if (null === $this->getDoctrine()->getRepository('Srm\UserBundle\Entity\User')->findOneById($contact->getContactId())) {
                 $user = new User($contact->getContactId(),$contact->getMail());
                 $user->setRole($this->getDoctrine()->getRepository('Srm\UserBundle\Entity\Role')->findOneByRoleType('ROLE_U'));
@@ -103,8 +111,14 @@ class ContactsController extends Controller
        } 
                 }
         }
-        return $this->redirect($this->generateUrl('srm_website_contacts_list', array(
+       if ($type=="interne"){ return $this->redirect($this->generateUrl('srm_website_contacts_list', array(
             'organisationId' => $organisation->getOrganisationId(),
+        )));}
+       else 
+            return $this->redirect($this->generateUrl('srm_website_stakeholders_list', array(
+            'organisationId' => $organisation->getOrganisationId(),
+                
         )));
+       
     }
 }
