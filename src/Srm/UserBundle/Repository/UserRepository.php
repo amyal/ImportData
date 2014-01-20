e26734a3f9cd6a1dccb2526b25c92b463d9d91f0
verseo <hichamelfassi+verseo@gmail.com>
2014-01-20 11:30:56 +0100

    URL(organisations/NumOfUser) requette (Organisation by User)

--- src/Srm/UserBundle/Repository/UserRepository.php
+++ src/Srm/UserBundle/Repository/UserRepository.php
@@ -5,7 +5,7 @@ namespace Srm\UserBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 use Srm\CoreBundle\Entity\Organisation;
-
+use Srm\CoreBundle\Entity\User;
 class UserRepository extends EntityRepository
 {
     public function findByContacts(array $contacts)
@@ -22,5 +22,23 @@ class UserRepository extends EntityRepository
             ->getQuery()
             ->getResult()
         ;
+    }
+    
+    //SELECT org.ORGANISATION_ID FROM v_organisation org JOIN v_contact con on org.ORGANISATION_ID = con.ORGANISATION_ID AND con.CONTACT_ID = 1
+    public function OrganisationByUser($user)
+    {      $q= $this->createQueryBuilder('u')
+               ->select('o.organisationId')
+            
+      ->innerJoin('u.contact', 'c')
+            ->innerJoin('c.organisation', 'o')  
+         ->Where('u.id = :userId')->setParameter('userId', $user->getid()) ;
+   // ->andWherewhere('c.isUser=true')*/
+    // $q = $this->createQueryBuilder('u')->select('u.id')->Where('u.username = :username')->setParameter('username', $userLogin) ->getQuery()->getResult() ;
+               return $q->getQuery()->getSingleScalarResult();
+        
+    // $query = $this->_em->createQuery('SELECT o.organisationId FROM user u JOIN u.contact c JOIN c.organisation o WHERE u.id = :id');
+     //$query->setParameter('id', $user);
+     //return $query->getSingleResult();
+    
     }
 }
