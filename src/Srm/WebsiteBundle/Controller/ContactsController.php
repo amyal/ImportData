d1bd860f53e86b84a503a8c946544b38d3153768
verseo <hichamelfassi+verseo@gmail.com>
2014-01-27 10:53:37 +0100

    Creation of Contact&User then send a message

--- src/Srm/WebsiteBundle/Controller/ContactsController.php
+++ src/Srm/WebsiteBundle/Controller/ContactsController.php
@@ -88,13 +88,13 @@ class ContactsController extends Controller
                 $contactName = $contact->getFirstname().' '.$contact->getLastname();
                 $user = new User($contact->getContactId(), $contactName, $contact->getMail());
                 $user->setRole($this->getDoctrine()->getRepository('Srm\UserBundle\Entity\Role')->findOneByRoleType('ROLE_U'));
-
+                $user->setContact($contact);
                 $encoder = $this->get('security.encoder_factory')->getEncoder($user);
                 $password = $encoder->encodePassword('toto', $user->getSalt());
                 $user->setPassword($password);
 
                 $this->get('fos_user.user_manager')->updateUser($user);
-                if ($contact->getParts()==Null ) { // if the user it's not a shareholder, then 
+        //      if ($contact->getParts()==Null ) { // if the user it's not a shareholder, then 
                     $message = \Swift_Message::newInstance() // we create a new instance of the Swift_Message class
                     ->setSubject('Verseo SRM') // we configure the title
                     ->setFrom('contact@verseo-consulting.com') // we configure the sender
@@ -103,11 +103,11 @@ class ContactsController extends Controller
                             ' '.$contact->getLastname() . ', votre Login est : ' .
                             $user->getUserName().' et votre Mot de passe est : toto');
                     // and we pass the $name variable to the text template which serves as a body of the message
-                    //$this->get('mailer')->send($message); // then we send the message.
+                    $this->get('mailer')->send($message); // then we send the message.
 
                     //     return new Response('Email bien envoyé');
-                } 
-            }
+           // }
+        }
         }
 
         if ($type == "externe"){  
