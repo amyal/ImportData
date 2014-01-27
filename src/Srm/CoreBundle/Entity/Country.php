2ee3c21080ea33c3f11d953337dadca17fdfa134
verseo <hichamelfassi+verseo@gmail.com>
2014-01-27 16:23:15 +0100

    add fields for countries in En/Fr

--- src/Srm/CoreBundle/Entity/Country.php
+++ src/Srm/CoreBundle/Entity/Country.php
@@ -16,8 +16,119 @@ class Country
      * @var integer
      */
     private $countryId;
+    
+    /**
+     * @var string
+     */
+    private $code;
 
+    /**
+     * @var string
+     */
+    private $alpha2;
+    
+    /**
+     * @var string
+     */
+    private $alpha3;
+    
+    /**
+     * @var string
+     */
+    private $label_an;
+    
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Country
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
 
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set alpha2
+     *
+     * @param string $alpha2
+     * @return Country
+     */
+    public function setAlpha2($alpha2)
+    {
+        $this->alpha2 = $alpha2;
+
+        return $this;
+    }
+
+    /**
+     * Get alpha2
+     *
+     * @return string
+     */
+    public function getAlpha2()
+    {
+        return $this->alpha2;
+    }
+
+    /**
+     * Set alpha3
+     *
+     * @param string $alpha3
+     * @return Country
+     */
+    public function setAlpha3($alpha3)
+    {
+        $this->alpha3 = $alpha3;
+
+        return $this;
+    }
+
+    /**
+     * Get alpha3
+     *
+     * @return string
+     */
+    public function getAlpha3()
+    {
+        return $this->alpha3;
+    }
+    
+    /**
+     * Set label_an
+     *
+     * @param string $label_an
+     * @return Country
+     */
+    public function setLabelAn($label_an)
+    {
+        $this->label_an = $label_an;
+
+        return $this;
+    }
+
+    /**
+     * Get label_an
+     *
+     * @return string
+     */
+    public function getLabelAn()
+    {
+        return $this->label_an;
+    }
+    
     /**
      * Set label
      *
@@ -40,7 +151,7 @@ class Country
     {
         return $this->label;
     }
-
+    
     /**
      * Get countryId
      *
