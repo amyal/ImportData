d04fd4834e4c6df7b7f3bb4b18834457d9cb13d8
rachidverseo <r_amyal@hotmail.com>
2014-03-04 09:15:53 +0100

    show validation panel by deleting 'novalidate'

--- src/Srm/IndicatorBundle/Controller/ReferencialsController.php
+++ src/Srm/IndicatorBundle/Controller/ReferencialsController.php
@@ -54,7 +54,7 @@ class ReferencialsController extends Controller
         $form = $this->createForm('srm_referencial', $referencial, array(
             'action' => $this->generateUrl($formActionRoute, $formActionRouteParams),
             'method' => 'POST',
-            'attr'   => array('class' => 'form-horizontal', 'novalidate' => 'novalidate'),
+            'attr'   => array('class' => 'form-horizontal'/*, 'novalidate' => 'novalidate'*/),
         ));
 
         $request = $this->getRequest();
