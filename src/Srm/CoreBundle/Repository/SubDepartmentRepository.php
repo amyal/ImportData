a85529369475b8684fdcfcb2d2c32cd620da7a04
rachidverseo <r_amyal@hotmail.com>
2014-03-27 11:13:40 +0100

    query return all SubDepartments that are not link with Department Bug ID in Mantis 0000013 review

--- src/Srm/CoreBundle/Repository/SubDepartmentRepository.php
+++ src/Srm/CoreBundle/Repository/SubDepartmentRepository.php
@@ -14,5 +14,22 @@ class SubDepartmentRepository extends EntityRepository
             ->getResult()
         ;
     }
+    // return all SubDepartments that are not link with Department 
+    public function getSDepartmentNotBind($org)
+    {   $sites= $this->getEntityManager()->getRepository('Srm\CoreBundle\Entity\Site')->findByOrganisation($org);
+        $departments = $this->getEntityManager()->getRepository('Srm\CoreBundle\Entity\Department')->findNonDeletedBySites($sites);
+                
+        $sdepartments = array();
 
+        foreach ($departments as $department) {
+            foreach ($department->getSubDepartments() as $sdepartment) {
+                $sdepartments[] = $sdepartment->getLabel();
+            }
+        }
+
+       $qb = $this->createQueryBuilder('sd')
+             ->where('sd.label not in (:sdSelected)')->setParameter('sdSelected',$sdepartments);
+       return $qb; 
+    }
+   
 }
